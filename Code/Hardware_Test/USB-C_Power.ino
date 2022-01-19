50dc549441706f52c7f3ae473dd5a0a3c5b63190
Michael Rankin <0miker0@gmail.com>
2022-01-19 15:07:51 -0400

    Updated sketch

--- Code/Hardware_Test/USB-C_Power.ino
+++ Code/Hardware_Test/USB-C_Power.ino
@@ -32,7 +32,46 @@ enum {
 };
 const uint8_t ucButtons[] = {25, 34, 32, 33, 35, 26}; // d-pad GPIO numbers
 
+int posCounter = 0;
+
 SPILCD lcd; // my display library
+
+struct stusb4500Params
+{
+
+  int voltInt;
+  float voltFloat;
+  int ampsInt;
+  float ampsFloat;
+  byte pdoNumber;
+  byte lowerTolerance;
+  byte upperTolerance;
+  const float fVolts[5] = { 5.0f, 9.0f, 12.0f, 15.0f, 20.0f};
+  const float fAmps[7] = {0.0f, 0.25f, 0.5f, 0.75f, 1.0f, 1.25f, 1.5f};
+};
+
+stusb4500Params tgt4500;
+//stusb4500Params tgt4500;
+const int ADC_AVG = 100;
+int adcReadings[ADC_AVG] = {};
+volatile int adcAvgPos;
+int adcAverage;
+
+
+
+
+volatile int interruptCounter;
+int totalInterruptCounter;
+
+hw_timer_t * timer = NULL;
+portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
+
+void IRAM_ATTR onTimer() {
+  portENTER_CRITICAL_ISR(&timerMux);
+  interruptCounter++;
+  portEXIT_CRITICAL_ISR(&timerMux);
+
+}
 
 int GetButtons(void)
 {
@@ -119,12 +158,17 @@ void setup() {
 
   float voltage, current;
   byte lowerTolerance, upperTolerance, pdoNumber;
+  tgt4500.pdoNumber = usb.getPdoNumber();
+  tgt4500.voltFloat = usb.getVoltage(tgt4500.pdoNumber);
+  tgt4500.ampsFloat = usb.getCurrent(tgt4500.pdoNumber);
+  tgt4500.lowerTolerance = usb.getLowerVoltageLimit(tgt4500.pdoNumber);
+  tgt4500.upperTolerance = usb.getUpperVoltageLimit(tgt4500.pdoNumber);
 
-  pdoNumber = usb.getPdoNumber();
-  voltage = usb.getVoltage(3);
-  current = usb.getCurrent(3);
-  lowerTolerance = usb.getLowerVoltageLimit(3);
-  upperTolerance = usb.getUpperVoltageLimit(3);
+  //pdoNumber = usb.getPdoNumber();
+  //  voltage = usb.getVoltage(3);
+  //  current = usb.getCurrent(3);
+  //  lowerTolerance = usb.getLowerVoltageLimit(3);
+  //  upperTolerance = usb.getUpperVoltageLimit(3);
 
 
   // Serial.begin(115200);
@@ -135,26 +179,35 @@ void setup() {
   spilcdWriteString(&lcd, 0, 0, (char *)"Mike's Power", 0x7e0, 0, FONT_12x16, DRAW_TO_LCD);
   spilcdWriteString(&lcd, 0, 16, (char *)" Controller", 0x7e0, 0, FONT_12x16, DRAW_TO_LCD);
   ButtonTest();
+
+  timer = timerBegin(0, 80, true);
+  timerAttachInterrupt(timer, &onTimer, true);
+  timerAlarmWrite(timer, 10000, true);
+  timerAlarmEnable(timer);
 }
 
-const char *szVolts[] = {"    ", "XXX  ", "5V  ", "9V  ", "12V ", "15V ", "20V ", "    "};
+const char *szVolts[] = {"    ",  "5V  ", "9V  ", "12V ", "15V ", "20V ", "    "};
 const char *szAmps[] = {"    ", "0.25A", "0.5A ", "0.75A", "1.0A ", "1.25A", "1.5A ", "    "};
 
-void DrawMenu(int iV, int iA, int iMenuPos)
+void DrawMenu(int iV, int iA, int iMenuPos, int iPos)
 {
   uint16_t usFG1 = (iMenuPos == 0) ? 0x1f : 0x14;
   uint16_t usFG2 = (iMenuPos == 1) ? 0x1f : 0x14;
   uint16_t dim;
   spilcdFill(&lcd, 0, DRAW_TO_LCD);
-#define MAX_POS 6
+
+
+
+#define MAX_POS 5
+  //http://www.rinkydinkelectronics.com/calc_rgb565.php
   dim = (usFG1 * 3) >> 2; // dim smaller text
   if (szVolts[iV - 1][0] != ' ') // some strange drawing error on the first line
     spilcdWriteStringCustom(&lcd, (GFXfont *)&Orbitron_Medium_16, 0, 23, (char *)szVolts[iV - 1], dim, 0, 1, DRAW_TO_LCD); //24
-  spilcdWriteStringCustom(&lcd, (GFXfont *)&Orbitron_Bold_22, 0, 45, (char *)szVolts[iV], usFG1, 0, 1, DRAW_TO_LCD);   //50
+  iPos ? spilcdWriteStringCustom(&lcd, (GFXfont *)&Orbitron_Bold_22, 0, 45, (char *)szVolts[iV], 0x3646, 0, 1, DRAW_TO_LCD) : spilcdWriteStringCustom(&lcd, (GFXfont *)&Orbitron_Bold_22, 0, 45, (char *)szVolts[iV], 0xC986, 0, 1, DRAW_TO_LCD);  //50
   spilcdWriteStringCustom(&lcd, (GFXfont *)&Orbitron_Medium_16, 0, 65, (char *)szVolts[iV + 1], dim, 0, 1, DRAW_TO_LCD); //74
   dim = (usFG2 * 3) >> 2; // dim smaller text
   spilcdWriteStringCustom(&lcd, (GFXfont *)&Orbitron_Medium_16, 70, 23, (char *)szAmps[iA - 1], dim, 0, 1, DRAW_TO_LCD); //35
-  spilcdWriteStringCustom(&lcd, (GFXfont *)&Orbitron_Bold_22, 70, 45, (char *)szAmps[iA], usFG2, 0, 1, DRAW_TO_LCD);   //50
+  iPos ? spilcdWriteStringCustom(&lcd, (GFXfont *)&Orbitron_Bold_22, 70, 45, (char *)szAmps[iA], 0x3646, 0, 1, DRAW_TO_LCD)  :   spilcdWriteStringCustom(&lcd, (GFXfont *)&Orbitron_Bold_22, 70, 45, (char *)szAmps[iA], 0xC986, 0, 1, DRAW_TO_LCD);   //50
   spilcdWriteStringCustom(&lcd, (GFXfont *)&Orbitron_Medium_16, 70, 65, (char *)szAmps[iA + 1], dim, 0, 1, DRAW_TO_LCD); //74
 } /* DrawMenu() */
 #define MAX_MENU 1
@@ -169,7 +222,7 @@ void initPD()
 
 void SetPower(int iP, int iV, int iA)
 {
-  const float fVolts[] = {0.0f, 5.0f, 5.0f, 9.0f, 12.0f, 15.0f, 20.0f};
+  const float fVolts[] = { 5.0f, 5.0f, 9.0f, 12.0f, 15.0f, 20.0f};
   const float fAmps[] = {0.0f, 0.25f, 0.5f, 0.75f, 1.0f, 1.25f, 1.5f};
   digitalWrite(Red_LED, !iP); // show the power on or off
   digitalWrite(Green_LED, iP);
@@ -184,10 +237,12 @@ void SetPower(int iP, int iV, int iA)
     usb.setUpperVoltageLimit(3, 20);
     usb.write();
     usb.softReset();
-  } else { // turn power off
+    spilcdRectangle(&lcd, 150, 0, 10, 80, 0x00FF00, 0, 1, DRAW_TO_LCD);
 
-    //Set PDO number to 1, which is default 5V. As setPowerAbove5VOnly is set (1), the VBUS_EN_SNK will not be driven, and Q1 will not conduct. 
-    //In other words, output *actually* goes off. 
+  } else { // turn power off
+    spilcdRectangle(&lcd, 150, 0, 10, 80, 0xFF0000, 0, 1, DRAW_TO_LCD);
+    //Set PDO number to 1, which is default 5V. As setPowerAbove5VOnly is set (1), the VBUS_EN_SNK will not be driven, and Q1 will not conduct.
+    //In other words, output *actually* goes off.
     usb.setPdoNumber(1);
     usb.write();
     usb.softReset();   //Enables Output
@@ -196,7 +251,6 @@ void SetPower(int iP, int iV, int iA)
 } /* SetPower() */
 void loop() {
 
-  char szTemp[32];  //NEW
 
 
   int iVolts = 1, iAmps = 1, iMenuPos = 0;
@@ -234,20 +288,94 @@ void loop() {
           SetPower(iPower, iVolts, iAmps);
           //Serial.println("bChanged");
         }
-        DrawMenu(iVolts, iAmps, iMenuPos);
+        DrawMenu(iVolts, iAmps, iMenuPos, iPower);
       } else if (iButtons & (0x1 << BUTT_POWER)) {
         iPower = !iPower;
         SetPower(iPower, iVolts, iAmps);
+        DrawMenu(iVolts, iAmps, iMenuPos, iPower);
+
         //Serial.println("buttPower (lol)");
       }
     } else {
-      delay(20); // waste a little time
-      spilcdWriteString(&lcd, 0, 0, (char *)" Voltage  Current", 0x7e0, 0, FONT_8x8, DRAW_TO_LCD); //Top
 
-      spilcdWriteString(&lcd, 0, 72, (char *)" Current Draw=", 0x7e0, 0, FONT_8x8, DRAW_TO_LCD); //Bottom
-      ADC_VALUE = analogRead(Analog_channel_pin);
-      sprintf(szTemp, "%d", ADC_VALUE);
-      spilcdWriteString(&lcd, 120, 72, szTemp, 0x7e0, 0, FONT_8x8, DRAW_TO_LCD);
+      if (interruptCounter > 1)
+      {
+        updateADCAvg();
+        interruptCounter = 0;
+        totalInterruptCounter++;
+      }
+      if (totalInterruptCounter > 1)
+
+      {
+
+        //the graph is *so* broken, don't draw the graph. 
+        //iPower ? drawGraph(adcAverage) : drawScreen(adcAverage);
+        
+        drawScreen(adcAverage);
+
+
+
+        totalInterruptCounter = 0;
+      }
     }
   }; // while (1)
 } /* loop() */
+void drawScreen(int adcVal)
+{
+  spilcdFill(&lcd, 0, DRAW_TO_LCD);
+  char szTemp[32];  //NEW
+
+  //delay(20); // waste a little time
+  spilcdWriteString(&lcd, 0, 0, (char *)" Voltage  Current", 0x7e0, 0, FONT_8x8, DRAW_TO_LCD); //Top
+
+  spilcdWriteString(&lcd, 0, 72, (char *)" Current Draw=", 0x7e0, 0, FONT_8x8, DRAW_TO_LCD); //Bottom
+  //ADC_VALUE = analogRead(Analog_channel_pin);
+  sprintf(szTemp, "%d", adcVal);
+  spilcdWriteString(&lcd, 120, 72, szTemp, 0x7e0, 0, FONT_8x8, DRAW_TO_LCD);
+}
+
+
+void drawGraph(int adcVal)
+{
+  int positionGap = 1;
+  int heightScale = adcVal / 10;
+
+
+
+  spilcdEllipse(&lcd, posCounter, (map(adcVal, 0, adcVal * 1.5, 0, 79)), 1, 1, 0xFFFFFF, 1, DRAW_TO_LCD);
+
+
+
+  char szTemp[32];  //NEW
+
+  //delay(20); // waste a little time
+  spilcdWriteString(&lcd, 0, 0, (char *)" Voltage  Current", 0x7e0, 0, FONT_8x8, DRAW_TO_LCD); //Top
+
+  spilcdWriteString(&lcd, 0, 72, (char *)" Current Draw=", 0x7e0, 0, FONT_8x8, DRAW_TO_LCD); //Bottom
+  //ADC_VALUE = analogRead(Analog_channel_pin);
+  sprintf(szTemp, "%d", adcVal);
+  spilcdWriteString(&lcd, 120, 72, szTemp, 0x7e0, 0, FONT_8x8, DRAW_TO_LCD);
+  posCounter++;
+  if (posCounter > 159)
+  {
+    posCounter = 0;
+    spilcdFill(&lcd, 0, DRAW_TO_LCD);
+
+  }
+}
+
+
+void updateADCAvg()
+{
+  if (adcAvgPos > ADC_AVG - 1)
+    adcAvgPos = 0;
+
+  adcReadings[adcAvgPos] = analogRead(Analog_channel_pin);
+  int total = 0;
+  for (int i = 0; i < ADC_AVG; i++)
+  {
+    total = total + adcReadings[i];
+  }
+  adcAverage = total / ADC_AVG;
+  adcAvgPos++;
+}
